dd35373cafe81dee7544267e44e478d2a32968f4
Ivan Guzman <ivan.guzman.art@gmail.com>
2021-07-27 23:54:40 -0700

    Updated font and button colors

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ export default function App() {
     <div className="app min-h-screen flex flex-col items-center justify-center text-center bg-gradient-to-r from-blue-400 to-purple-400 leading-6 px-8">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10 border border-gray-200 py-8 px-4 rounded sm:rounded-lg shadow-2xl">
-          <h2 className="text-6xl text-indigo-900 mb-5 font-mono">
-            Pomo Focus
+          <h2 className="text-6xl text-indigo-100 mb-5 font-mono">
+            Pomodoro Focus
           </h2>
 
-          <div className="timer text-7xl text-indigo-700 mb-3 font-mono">
+          <div className="timer text-7xl text-indigo-100 mb-3 font-mono">
             <span>00</span>
             <span>:</span>
             <span>00</span>
@@ -19,19 +19,19 @@ export default function App() {
           <span className="buttons relative z-0 inline-flex shadow-sm rounded-md mt-3 font-mono ">
             <button
               type="button"
-              className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-purple-50 text-sm font-medium text-indigo-700 hover:bg-purple-100 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+              className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-indigo-700 text-sm font-medium text-indigo-100 hover:bg-purple-800 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
             >
               Start
             </button>
             <button
               type="button"
-              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-purple-50 text-sm font-medium text-indigo-700 hover:bg-purple-100 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+              className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-indigo-700 text-sm font-medium text-indigo-100 hover:bg-purple-800 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
             >
               Stop
             </button>
             <button
               type="button"
-              className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-purple-50 text-sm font-medium text-indigo-700 hover:bg-purple-100 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+              className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-indigo-700 text-sm font-medium text-indigo-100 hover:bg-purple-800 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
             >
               Reset
             </button>
